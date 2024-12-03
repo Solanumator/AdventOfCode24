04c255ff628e60f9a6040f898be2fcced46d1d86
Connor Bradley <cbradley857@gmail.com>
2024-12-03 13:25:01 +0000

    Alternative method of part 2 (little/no perf difference)

--- Days/Day3.cs
+++ Days/Day3.cs
@@ -30,6 +30,10 @@ namespace AdventOfCode24.Days
         public void Part2() =>
             TimedTest(() => Console.WriteLine(this.lines.Sum(x => this.MultiplyRow_Part2(x))));
 
+        [Test]
+        public void Part2_Alt() =>
+            TimedTest(() => Console.WriteLine(this.MultiplyRow_Part2_Split()));
+
         private int MultiplyRow(string row)
         {
             var res = 0;
@@ -87,6 +91,28 @@ namespace AdventOfCode24.Days
             return res;
         }
 
+        private int MultiplyRow_Part2_Split()
+        {
+            var res = 0;
+
+            // Join into one string
+            var s = string.Join(string.Empty, this.lines);
+
+            // Loop each section where mult applies
+            foreach (Match section in Part2ExtendedRegex().Matches(s))
+            {
+                foreach (Match m in AllInOneRegex().Matches(section.Value))
+                {
+                    var num1 = int.Parse(m.Groups["num1"].Value);
+                    var num2 = int.Parse(m.Groups["num2"].Value);
+
+                    res += (num1 * num2);
+                }
+            }
+
+            return res;
+        }
+
         [GeneratedRegex(@"mul\((?<num1>[0-9]{1,3}),(?<num2>[0-9]{1,3})\)")]
         private static partial Regex AllInOneRegex();
 
@@ -98,5 +124,9 @@ namespace AdventOfCode24.Days
 
         [GeneratedRegex(@"(?<mul>mul\((?<num1>[0-9]{1,3})),(?<num2>[0-9]{1,3})\)|(?<dont>don't\(\))|(?<do>do\(\))")]
         private static partial Regex Part2Regex();
+
+        // Gets all sections where the mult applies
+        [GeneratedRegex(@"^(.*?)(?=don't\(\))|do\(\)(.*?)(?=don't\(\)|$)")]
+        private static partial Regex Part2ExtendedRegex();
     }
 }
